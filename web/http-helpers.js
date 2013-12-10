f67aaeb9acf8917e561c6ce187dc4b5334370671
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-12-10 15:04:42 -0800

    refactored code

--- web/http-helpers.js
+++ web/http-helpers.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 var qs = require('querystring');
 var rq = require('./request-handler');
 var mysql = require('mysql');
+var webRoot = path.join(__dirname, './public/');
 var connection = mysql.createConnection({
   host : 'localhost',
   user : 'root'
@@ -33,46 +34,40 @@ exports.serveStaticAssets = function(res, folder, asset) {
   });
 };
 
-exports.writeData = function(data){
-  url = qs.parse(data).url;
+exports.writeData = function(url){
   connection.query('insert into archive.runList (url) values ('+ connection.escape(url)+ ')');
   // fs.appendFile(rq.datadir, url + '\n');
 };
 
 // As you go through, keep thinking about what helper functions you can put here!
-exports.checkReadWrite = function(res, folder, asset) {
+exports.findSite = function(url, res, responseCodes) {
   var html = '';
-  var url = qs.parse(asset).url;
+  console.log(connection.escape(url));
   connection.query('select * from archive.runList WHERE url = ' + connection.escape(url), function(err,rows){
-    if (err){
-        exports.writeData(asset);
-        res.writeHead(302, headers);
-        exports.serveStaticAssets(res, path.join(__dirname, "./public/"), "index.html");
+    if (err || !rows[0]){
+      console.log(rows);
+      //if errror querieng then for post case we'll write Data
+      if (responseCodes[1] === 302) { exports.writeData(url); }
+      res.writeHead(responseCodes[1], headers);
+      exports.serveStaticAssets(res, webRoot, "index.html");
     } else {
-        console.log(rows[0].ID);
-        connection.query('select * from archive.webHistory WHERE runID = ' + rows[0].ID + ' ORDER BY created DESC LIMIT 1', function(err, data) {
-          if (err){
-            console.log(err);
-          } else {
-            console.log(data);
-            res.writeHead(200, headers);
-            res.end(data[0].data);
-          }
-        });
-      }
+      connection.query('select * from archive.webHistory WHERE runID = ' + rows[0].ID + ' ORDER BY created DESC LIMIT 1', function(err, data) {
+        if (err){
+          res.writeHead(responseCodes[1], headers);
+          res.end('this page is in the archive queue');
+        } else {
+          res.writeHead(responseCodes[0], headers);
+          res.end(data[0].data);
+        }
+      });
+    }
   });
-  // fs.readFile(folder + qs.parse(asset).url, 'utf8', function(err, data) {
-  //   html += data;
-  //   if (err){
-  //     exports.writeData(asset);
-  //     res.writeHead(302, headers);
-  //     exports.serveStaticAssets(res, path.join(__dirname, "./public/"), "index.html");
-  //   } else{
-  //     res.writeHead(200, headers);
-  //     res.end(html);
-    // }
-  // });
 };
+
+exports.getFromDB = function(req, res) {
+  exports.findSite(req.url, res, [200, 404]);
+};
+
 
 exports.getNewSites = function(res){
   fs.readFile(path.join(__dirname,"../data/") + "sites.txt", function(err, data) {
@@ -83,3 +78,42 @@ exports.getNewSites = function(res){
     res.end(JSON.stringify(urls));
   });
 };
+
+exports.handlePost = function(req,res){
+  url = "";
+  req.on('data',function(data, err){
+    if (err) { throw err; }
+    url += data;
+  });
+  req.on('end', function(){
+    exports.findSite(qs.parse(url).url, res, [200, 302] );
+  });
+};
+exports.getFromFile = function(req,res){
+  exports.serveStaticAssets(res,webRoot,req.url);
+};
+
+exports.indexSiteListOr404 = function (req,res){
+  if (req.url === '/') {
+    exports.serveStaticAssets(res,webRoot,'index.html');
+  } else if (req.url === '/newsites'){
+    exports.getNewSites(res);
+  } else {
+    res.writeHead(404, headers);
+    res.end("page not found");
+  }
+};
+
+// fs.readFile(folder + qs.parse(asset).url, 'utf8', function(err, data) {
+  //   html += data;
+  //   if (err){
+  //     exports.writeData(asset);
+  //     res.writeHead(302, headers);
+  //     exports.serveStaticAssets(res, path.join(__dirname, "./public/"), "index.html");
+  //   } else{
+  //     res.writeHead(200, headers);
+  //     res.end(html);
+    // }
+  // });
+
+
--- web/request-handler.js
+++ web/request-handler.js
@@ -1,41 +1,25 @@
 var path = require('path');
 var halp = require('./http-helpers');
 module.exports.datadir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
-
-
-
+var router = {
+  "GET" : {
+    "" : halp.indexSiteListOr404,
+    ".css" : halp.getFromFile,
+    ".html" : halp.getFromFile,
+    ".js" : halp.getFromFile
+  },
+  "POST" : {
+    "" : halp.handlePost
+  }
+};
 
 module.exports.handleRequest = function (req, res) {
-  if (req.method === 'POST') {
-    url = "";
-    req.on('data',function(data, err){
-      if (err) { throw err; }
-      url += data;
-    });
-    req.on('end', function(){
-      console.log('url');
-      halp.checkReadWrite(res, path.join(__dirname, "../data/sites/"), url);
-    });
-  } else if (req.method === 'GET') {
-    var ext = path.extname(req.url);
-    console.log(req.url);
-    var folder = "";
-    if (req.url === '/') {
-      folder = './public/';
-      req.url = 'index.html';
-    }else if ( ext === '.html' || ext === '.css' || ext === '.js' || ext === '.jpg'){
-      folder = './public/';
-    } else if (ext === ""){
-        if (req.url === "/newsites"){
-          halp.getNewSites(res);
-        } else {
-          res.writeHead(404, halp.headers);
-          res.end();
-        }
-      return;
-    }else {
-      folder = "../data/sites/";
-    }
-    halp.serveStaticAssets(res, path.join(__dirname, folder),  req.url);
+  var ext = path.extname(req.url);
+  console.log(ext);
+  var route = router[req.method][ext];
+  if (!route){
+    halp.getFromDB(req,res);
+  } else {
+    route(req,res);
   }
 };
