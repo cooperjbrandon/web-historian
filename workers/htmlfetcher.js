9c27ffcf91dd712ff189a7f479c2fc25ac883faf
Hack Reactor Students <cooperjbrandon@gmail.com>
2013-12-09 17:13:32 -0800

    get html from websites

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,7 @@
-// eventually, you'll have some code here that uses the tested helpers 
+var path = require('path');
+var dir = path.join(__dirname, "../data/sites.txt"); // tests will need to override this.
+var halp = require('./lib/html-fetcher-helpers.js');
+
+halp.readUrls(dir, halp.downloadUrls);
+// eventually, you'll have some code here that uses the tested helpers
 // to actually download the urls you want to download.
--- workers/lib/html-fetcher-helpers.js
+++ workers/lib/html-fetcher-helpers.js
@@ -1,7 +1,27 @@
-exports.readUrls = function(filePath, cb){
+var http = require('http-request');
+var fs = require('fs');
+var path = require('path');
+var dir = path.join(__dirname, "../../data/sites/"); // tests will need to override this.
+console.log(dir);
 
+exports.readUrls = function(filePath, cb){
+  fs.readFile(filePath, function(err, data) {
+    if (err) { throw err ;}
+    data = data + "";
+    var urls = data.split("\n");
+    cb(urls);
+  });
 };
 
 exports.downloadUrls = function(urls){
-  // fixme
+  for (var i = 0 ; i < urls.length ; i++){
+    exports.fetchSite(urls[i]);
+  }
+};
+
+exports.fetchSite = function(url){
+  http.get(url, function (err, res) {
+    console.log(dir+url);
+    fs.writeFile(dir + url, res.buffer + "",'utf8');
+  });
 };
